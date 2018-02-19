151b8a71a9717688d336922d04fb51411fba2564
Freddy Manrique <fredar21@gmail.com>
2018-02-19 18:23:17 -0500

    Install vector icons

--- App.js
+++ App.js
@@ -25,10 +25,10 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native con sachi mi mamas!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          To get started, edit App.js con sachi
         </Text>
         <Text style={styles.instructions}>
           {instructions}
