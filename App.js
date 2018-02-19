c8b4983e7c6da9151e0bce63d81c690a03edc071
Freddy Manrique <fredar21@gmail.com>
2018-02-19 18:42:04 -0500

    box

--- App.js
+++ App.js
@@ -9,30 +9,27 @@ import {
   Platform,
   StyleSheet,
   Text,
+  Image,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+
+    const image = "http://1.bp.blogspot.com/-qNg0y-A3foM/UMIbLV4P46I/AAAAAAAAT-g/TlcuaehdSSU/s1600/9-11-12-rescate_ad33362bd4aba2fd3d6fdda7217a3630.jpg";
+    const name = "Rescate Band";
+
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native con sachi mi mamas!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js con sachi
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.artistBox}>
+          <Image style={styles.image} source={{uri: image }}></Image>
+          <View style={styles.info}>
+          <Text style={styles.name}>{name}</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -41,18 +38,23 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'lightgrey',
   },
-  welcome: {
+  image:{
+    width: 150,
+    height: 150
+  },
+  artistBox:{
+    flexDirection: "row",
+    backgroundColor: "white",
+  },
+  info:{
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  name:{
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    marginTop: 10,
+  }
 });
